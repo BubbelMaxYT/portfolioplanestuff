f84fd713f0d59d79d8b7475839a95fe91d396d4c
BubbelMaxYT <97031631+BubbelMaxYT@users.noreply.github.com>
2026-02-07 00:39:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-window.addEventListener("scroll", () => {
-  document.querySelectorAll("section").forEach(section => {
-    const top = window.scrollY;
-    const offset = section.offsetTop - 400;
-    const height = section.offsetHeight;
-    if (top >= offset && top < offset + height) {
-      section.classList.add("visible");
-    }
-  });
-});
+
